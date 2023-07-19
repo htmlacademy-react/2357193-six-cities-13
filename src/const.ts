4cf78196e8626b31c6ad5ea8706ce04569765aa3
Казыханов Ренат Айбулатович <rakazykhan@mts.ru>
2023-07-20 01:23:30 +0500

    3.Маршрут по тайным тропам

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+export enum AppRoute {
+    Root = '/',
+    Login = '/login',
+    Offer = '/offer/:id',
+    Favorites = '/favorites',
+  }
+
+export enum AuthorizationStatus {
+    Auth = 'AUTH',
+    NoAuth = 'NO_AUTH',
+    Unknown = 'UNKNOWN',
+}
+
